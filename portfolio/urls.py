1e2c0202a41882f59f59b6a45bf8ca0b93b6caa6
Sam <sam.eskandar00@hotmail.com>
2020-01-08 22:31:47 -0500

    finished front end for 'projects'

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('navigation/', views.navigation, name='navigation'),
     path('blog/', views.blog, name='blog'),
+    path('projects/', views.projects, name='projects'),
+    path('claw_machine/', views.claw_machine, name='claw_machine'),
+    path('coollistings/', views.coollistings, name='coollistings'),
+    path('wheel_of_fortune/', views.wheel_of_fortune, name='wheel_of_fortune'),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -12,3 +12,19 @@ def navigation(request):
 
 def blog(request):
     return render(request, 'blog.html')
+
+
+def projects(request):
+    return render(request, 'projects.html')
+
+
+def claw_machine(request):
+    return render(request, 'claw_machine.html')
+
+
+def coollistings(request):
+    return render(request, 'coollistings.html')
+
+
+def wheel_of_fortune(request):
+    return render(request, 'wof.html')
